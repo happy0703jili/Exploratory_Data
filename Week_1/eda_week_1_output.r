5dad59f82470f45a9cb36e229cf8c2a76b5fc38d
Li JI <happy0703jili@qq.com>
2016-07-25 09:18:47 +0200

    Update eda_week_1_output.r

--- Week_1/eda_week_1_output.r
+++ Week_1/eda_week_1_output.r
@@ -1,7 +1,8 @@
 library("dplyr")
-types <- c("character", "character",rep("numeric",7))
+# import the dataset
 e_power_con <- read.delim("E:/MOOC/exploratory data analysis/household_power_consumption.txt", header = T,sep = ";", stringsAsFactors = F)
 
+# data manipulation
 e_power_con_2d <- e_power_con %>%
   filter(Date == "1/2/2007" | Date == "2/2/2007") %>%
   mutate(Date_Time = paste(Date,Time)) %>%
@@ -10,6 +11,7 @@ e_power_con_2d <- e_power_con %>%
 e_power_con_2d$Date_Time <- as.POSIXct(e_power_con_2d$Date_Time, format="%d/%m/%Y %H:%M:%S")
 e_power_con_2d$Time <- as.POSIXct(e_power_con_2d$Time, format="%d/%m/%Y %H:%M:%S")
 
+# change the type of the data in row 3 to 9
 for (i in 3:9) {
   e_power_con_2d[,i] <- as.numeric(e_power_con_2d[,i])
 } 
